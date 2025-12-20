0006058f88aeeea25e57bde0a76a7bb7262b44c9
abc <abc@email.com>
2025-12-20 13:15:26 +0530

    fix: PlaywrightService

--- Services/Implementations/PlaywrightService.cs
+++ Services/Implementations/PlaywrightService.cs
@@ -1,18 +1,20 @@
-﻿using GoIoFish.Services.Interfaces;
-using Microsoft.Playwright;
-using System;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
+using GoIoFish.Services.Interfaces;
+using Microsoft.Playwright;
 
 namespace GoIoFish.Services.Implementations
 {
     public class PlaywrightService : IPlaywrightService
     {
-
         private IPage _page;
         public event Action Unavailable;
 
-        public PlaywrightService() { }
+        public PlaywrightService()
+        {
+        }
 
         public async Task InitAsync()
         {
@@ -22,7 +24,7 @@ namespace GoIoFish.Services.Implementations
                 "GoIoFish", "Google", "Chrome", "User Data");
             var context = await playwright.Chromium.LaunchPersistentContextAsync(userDataDir, new BrowserTypeLaunchPersistentContextOptions
             {
-                Headless = true,
+                Headless = !Debugger.IsAttached,
                 Args = new[] { "--start-maximized", "--auto-open-devtools-for-tabs" },
                 ExecutablePath = @"C:\Program Files\Google\Chrome\Application\chrome.exe",
                 ViewportSize = ViewportSize.NoViewport,
@@ -32,7 +34,10 @@ namespace GoIoFish.Services.Implementations
 
             _page = page;
             _page.Close += async (sender, e) => await DisposeAsync();
-            _page.Context.Browser.Disconnected += async (sender, e) => await DisposeAsync();
+            if (_page.Context.Browser != null)
+            {
+                _page.Context.Browser.Disconnected += async (sender, e) => await DisposeAsync();
+            }
         }
 
         public IPage GetPage()
@@ -43,7 +48,10 @@ namespace GoIoFish.Services.Implementations
         public async ValueTask DisposeAsync()
         {
             Unavailable?.Invoke();
-            await _page.Context.Browser?.CloseAsync();
+            if (_page.Context.Browser != null)
+            {
+                await _page.Context.Browser.CloseAsync();
+            }
         }
     }
 }
