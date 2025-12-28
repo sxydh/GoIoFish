d7c8785d8f3506022de8f20e4a30a6f64fb60d13
abc <abc@email.com>
2025-12-28 11:50:14 +0800

    fix: IPlaywrightActorService

---
+++ Services/Interfaces/IPlaywrightActorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace GoIoFish.Services.Interfaces
+{
+    public interface IPlaywrightActorService : IAsyncDisposable
+    {
+        Task InitAsync();
+        Task EnqueueAsync(Func<IPage, Task> action);
+        bool IsAvailable { get; }
+    }
+}
