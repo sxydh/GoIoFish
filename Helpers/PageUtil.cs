ac6d9a81223701161e207b8ba6c30202e2b166be
abc <abc@email.com>
2026-01-02 00:26:14 +0800

    fix: PageUtil

--- Helpers/PageUtil.cs
+++ Helpers/PageUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Playwright;
 
 namespace GoIoFish.Helpers
@@ -34,6 +35,29 @@ namespace GoIoFish.Helpers
             await ExUtil.SafeExecAsync(
                 async () => await page.Locator(selector).ClickAsync(new LocatorClickOptions { Timeout = timeout }),
                 isThrow, msg);
+        }
+
+        public static async Task<IFrame> GetFrameAsync(this IPage page, string selector, long timeout = 60000, bool isThrow = true, string msg = null)
+        {
+            await page.WaitForAsync(selector, timeout: timeout, isThrow: isThrow, msg: msg);
+            await Task.Delay(1000);
+            return await ExUtil.SafeExecAsync(
+                async () =>
+                {
+                    var ele = await page.QuerySelectorAsync(selector) ?? throw new NullReferenceException();
+                    return await ele.ContentFrameAsync();
+                },
+                isThrow, msg);
+        }
+
+        public static async Task WaitForAsync(this IFrame frame, string selector, long timeout = 60000, bool isThrow = true, string msg = null)
+        {
+            await WaitForAsync(frame.Locator(selector), timeout: timeout, isThrow: isThrow, msg: msg);
+        }
+
+        public static async Task WaitForVisibleAsync(this IFrame frame, string selector, long timeout = 60000, bool isThrow = true, string msg = null)
+        {
+            await WaitForAsync(frame.Locator(selector), state: WaitForSelectorState.Visible, timeout: timeout, isThrow: isThrow, msg: msg);
         }
     }
 }
