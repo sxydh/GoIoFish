bf74901f473240fae4d7d6a8b057c05ff2671ae0
abc <abc@email.com>
2026-01-01 20:19:22 +0800

    feat: ExUtil

---
+++ Helpers/ExUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GoIoFish.Helpers
+{
+    public static class ExUtil
+    {
+        public static void SafeExe(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        public static async Task SafeExeAsync(Func<Task> asyncAction)
+        {
+            try
+            {
+                await asyncAction();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
