d44b12c841634440001d68d01a49bfa3ff16b97c
abc <abc@email.com>
2026-01-02 00:30:58 +0800

    fix: LoginService

--- Services/Implementations/LoginService.cs
+++ Services/Implementations/LoginService.cs
@@ -24,15 +24,15 @@ namespace GoIoFish.Services.Implementations
         {
             await page.GotoAsync("https://www.goofish.com/", msg: "进入网址超时");
             await page.WaitForVisibleAsync("//div[text()='登录']", msg: "进入首页超时");
-            await Task.Delay(1000);
+            await Task.Delay(3000);
 
             await page.ClickAsync("//div[text()='登录']", msg: "点击登录超时");
+            var qrCodeFrame = await page.GetFrameAsync("//iframe[@id='alibaba-login-box']", timeout: 6000, msg: "获取登录二维码框架超时");
             const string qrCodeSelector = "//div[@id='qrcode-img']/canvas";
-            await page.WaitForVisibleAsync(qrCodeSelector, msg: "登录二维码超时");
-            var qrCodeCanvas = await page.QuerySelectorAsync(qrCodeSelector) ?? throw new Exception("获取登录二维码异常");
+            await qrCodeFrame.WaitForAsync(qrCodeSelector, timeout: 6000, msg: "获取登录二维码超时");
+            var qrCodeCanvas = await qrCodeFrame.QuerySelectorAsync(qrCodeSelector) ?? throw new Exception("获取登录二维码异常");
             var qrCodeCanvasBuffer = await qrCodeCanvas.ScreenshotAsync();
             var qrCodeBase64 = Convert.ToBase64String(qrCodeCanvasBuffer);
-            Console.WriteLine(qrCodeBase64);
         }
     }
 }
