46c00a81cd1d6babdd66b429b80bbb2840a98d57
abc <abc@email.com>
2026-01-01 23:10:44 +0800

    fix: PageUtil

--- Helpers/PageUtil.cs
+++ Helpers/PageUtil.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Playwright;
 
 namespace GoIoFish.Helpers
@@ -8,17 +7,21 @@ namespace GoIoFish.Helpers
     {
         public static async Task GotoAsync(this IPage page, string url, long timeout = 60000, bool isThrow = true, string msg = null)
         {
-            try
-            {
-                await page.GotoAsync(url, new PageGotoOptions { Timeout = timeout });
-            }
-            catch (Exception e)
-            {
-                if (isThrow)
-                {
-                    throw new Exception(msg ?? e.Message, e);
-                }
-            }
+            await ExUtil.SafeExecAsync(
+                async () => await page.GotoAsync(url, new PageGotoOptions { Timeout = timeout }),
+                isThrow, msg);
+        }
+
+        public static async Task WaitForAsync(this IPage page, string selector, long timeout = 60000, bool isThrow = true, string msg = null)
+        {
+            await WaitForAsync(page.Locator(selector), timeout: timeout, isThrow: isThrow, msg: msg);
+        }
+
+        public static async Task WaitForAsync(this ILocator locator, WaitForSelectorState state = WaitForSelectorState.Attached, long timeout = 60000, bool isThrow = true, string msg = null)
+        {
+            await ExUtil.SafeExecAsync(
+                async () => await locator.WaitForAsync(new LocatorWaitForOptions { State = state, Timeout = timeout }),
+                isThrow, msg);
         }
     }
 }
