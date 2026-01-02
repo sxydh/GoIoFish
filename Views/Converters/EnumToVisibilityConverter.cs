22f35cc9fe581bd1e71dc53cad6df06c59558f15
abc <abc@email.com>
2026-01-02 20:38:51 +0800

    fix: EnumToVisibilityConverter

--- Views/Converters/EnumToVisibilityConverter.cs
+++ Views/Converters/EnumToVisibilityConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace GoIoFish.Helpers
+namespace GoIoFish.Views.Converters
 {
     public class EnumToVisibilityConverter : IValueConverter
     {
