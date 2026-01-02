bcc9a17e6c1d23137c0e6c1779ab72bd751d8f93
abc <abc@email.com>
2026-01-02 22:40:06 +0800

    fix: LoginViewModel

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
-using GoIoFish.Helpers;
 using GoIoFish.Models;
 using GoIoFish.Services.Interfaces;
+using GoIoFish.Views.Pages;
 
 namespace GoIoFish.ViewModels
 {
@@ -12,30 +11,11 @@ namespace GoIoFish.ViewModels
         private readonly INavigationService _navigationService;
         private readonly ILoginService _loginService;
         private LoginState _state;
-        private string _qrCode;
-        private BitmapImage _qrCodeImage;
 
         public LoginState State
         {
             get => _state;
             set => SetProperty(ref _state, value);
-        }
-
-        public string QrCode
-        {
-            set
-            {
-                if (SetProperty(ref _qrCode, value))
-                {
-                    QrCodeImage = ImageUtil.SafeBase64ToBitmap(value);
-                }
-            }
-        }
-
-        public BitmapImage QrCodeImage
-        {
-            get => _qrCodeImage;
-            private set => SetProperty(ref _qrCodeImage, value);
         }
 
         public LoginViewModel(INavigationService navigationService, ILoginService loginService)
@@ -47,17 +27,12 @@ namespace GoIoFish.ViewModels
         public async Task InitAsync()
         {
             State = LoginState.LoadingPage;
-            await _loginService.LoginAsync(new Progress<LoginProgressMsg>(e =>
+            var isLoginOk = await _loginService.LoginAsync(new Progress<LoginProgressMsg>(e => { State = e.State; }));
+            if (isLoginOk)
             {
-                State = e.State;
-                switch (e.State)
-                {
-                    case LoginState.QrCodeReady:
-                        QrCode = e.Body;
-                        break;
-                }
-            }));
-            State = LoginState.LoginSucceeded;
+                State = LoginState.LoginSucceeded;
+                _navigationService.NavigateTo<HomePage>();
+            }
         }
     }
 }
