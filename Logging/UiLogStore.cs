54d4c955a700fc91d2d9ee210751f2076b3f7f37
abc <abc@email.com>
2026-01-02 16:55:02 +0800

    fix: UiLogStore

---
+++ Logging/UiLogStore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace GoIoFish.Logging
+{
+    public static class UiLogStore
+    {
+        public static ObservableCollection<LogItem> Logs { get; } = new ObservableCollection<LogItem>();
+
+        public static void Add(string message, string level = "Info")
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Logs.Add(new LogItem { Message = message, Level = level });
+                if (Logs.Count > 500) Logs.RemoveAt(0);
+            });
+        }
+    }
+
+    public class LogItem
+    {
+        public string Message { get; set; }
+        public string Level { get; set; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,26 @@
-﻿using GoIoFish.Services.Implementations;
+﻿using System.Windows;
+using GoIoFish.Services.Implementations;
 using GoIoFish.Services.Interfaces;
 using GoIoFish.ViewModels;
 using GoIoFish.Views.Pages;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Windows;
+using NLog;
 
 namespace GoIoFish
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
         public MainWindow()
         {
             InitializeComponent();
 
             // 注册 Pages
+            Log.Debug("注册 Pages...");
             var services = new ServiceCollection();
             services.AddTransient<MainViewModel>();
             services.AddTransient<LoginPage>();
@@ -27,6 +29,7 @@ namespace GoIoFish
             services.AddTransient<HomeViewModel>();
 
             // 注册 Services
+            Log.Debug("注册 Services...");
             services.AddSingleton<INavigationService>(sp => new NavigationService(MainFrame, sp));
             services.AddSingleton<ILoginService, LoginService>();
             services.AddSingleton<IPlaywrightActorService, PlaywrightActorService>();
@@ -37,10 +40,10 @@ namespace GoIoFish
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
             Loaded += (sender, args) =>
             {
+                Log.Debug("MainWindow loaded...");
                 var navigationService = serviceProvider.GetRequiredService<INavigationService>();
                 navigationService.NavigateTo<LoginPage>();
             };
-
         }
     }
 }
