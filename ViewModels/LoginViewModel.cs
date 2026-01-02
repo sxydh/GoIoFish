48e08765e67593167c3a6138fa8751efb7b09bbd
abc <abc@email.com>
2026-01-02 17:57:08 +0800

    fix: LoginViewModel

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,40 +1,24 @@
-﻿using GoIoFish.Helpers;
+﻿using System.Threading.Tasks;
 using GoIoFish.Services.Interfaces;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace GoIoFish.ViewModels
 {
-    public class LoginViewModel : INotifyPropertyChanged
+    public class LoginViewModel : ViewModelBase
     {
-
         private readonly INavigationService _navigationService;
         private readonly ILoginService _loginService;
         private bool _isLoading;
+
         public bool IsLoading
         {
             get => _isLoading;
-            set
-            {
-                _isLoading = value;
-                OnPropertyChanged();
-            }
+            set => SetProperty(ref _isLoading, value);
         }
-        public ICommand LoginCommand { get; }
 
         public LoginViewModel(INavigationService navigationService, ILoginService loginService)
         {
             _navigationService = navigationService;
             _loginService = loginService;
-            LoginCommand = new RelayCommand(() => { });
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         public async Task InitAsync()
@@ -43,6 +27,5 @@ namespace GoIoFish.ViewModels
             await _loginService.LoginAsync();
             IsLoading = false;
         }
-
     }
 }
