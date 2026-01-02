49bf3050bc18e08433f9eb0086ae39a710f3f314
abc <abc@email.com>
2026-01-02 20:20:27 +0800

    feat: LoginProgressMsg

---
+++ Models/LoginEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace GoIoFish.Models
+{
+    public class LoginProgressMsg
+    {
+        public LoginState State { get; set; }
+        public string Body { get; set; }
+
+        public LoginProgressMsg(LoginState state, string body)
+        {
+            State = state;
+            Body = body;
+        }
+    }
+
+    public enum LoginState
+    {
+        LoadingPage,
+        QrCodeReady,
+        LoginSucceeded,
+    }
+}
