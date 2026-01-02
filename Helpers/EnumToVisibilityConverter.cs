b3c21ccf3a9082487b823250a1ba362e7ae00993
abc <abc@email.com>
2026-01-02 20:19:32 +0800

    feat: EnumToVisibilityConverter

---
+++ Helpers/EnumToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GoIoFish.Helpers
+{
+    public class EnumToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null) return Visibility.Collapsed;
+            var enumName = value.ToString();
+            var targetName = parameter.ToString();
+            return enumName == targetName ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
